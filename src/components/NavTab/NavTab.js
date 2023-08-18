2e60948018b721382ec4d6dc89ad65509eeaaa37
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-08-18 14:14:17 +0300

    create AboutProject component, add in Main

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -5,7 +5,7 @@ function NavTab() {
     return (
         <section className="navTab">
             <nav className="navTab__links">
-                <a href="#aboutProject" className="navTab__link">О проекте</a>
+                <a href="#about-project" className="navTab__link">О проекте</a>
                 <a href="#techs" className="navTab__link">Технологии</a>
                 <a href="#student" className="navTab__link">Студент</a>
         </nav>
