f636a2e405d1d548a235dc22dc457cb0c03888ed
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-08-28 20:12:30 +0300

    create FilterCheckbox, Preloader Components

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './SearchForm.css';
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function SearchForm(props) {
     return (
             <section className="search-form">
                 <div className="search-form__container">
-                    <input className="search-form__input"></input>
+                    <input className="search-form__input" placeholder="Фильм" required></input>
                     <button className="search-form__button"></button>                   
                 </div>
-                {/* <FilterCheckbox /> */}
+                <FilterCheckbox />
             </section>
     )
 }
